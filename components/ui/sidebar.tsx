c6637cb8788a1b0e2e71eedaf19508f4c96466b4
rupesh <rpiwal02@gmail.com>
2026-05-03 20:31:30 +0530

    fix(sidebar): resolve TooltipTrigger type error by using asChild pattern

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -522,7 +522,7 @@ function SidebarMenuButton({
       },
       props
     ),
-    render: !tooltip ? render : <TooltipTrigger render={render} />,
+    render: !tooltip ? render : <TooltipTrigger asChild>{render as any}</TooltipTrigger>,
     state: {
       slot: "sidebar-menu-button",
       sidebar: "menu-button",
